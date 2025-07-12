68bc4fc566503f8d37de16b000b8afd52b372a64
Omer Paz <omerpaz28@gmail.com>
2025-07-12 18:50:59 +0300

    Updated test

--- backend/tests/crud.test.ts
+++ backend/tests/crud.test.ts
@@ -1,9 +1,9 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
-import { createApp } from '../src/expressApp';
+//import { createApp } from '../src/expressApp';
 import { connectMongo } from '../config/mongo';
 
-const app = createApp();
+//const app = createApp();
 let createdId: string;
 
 beforeAll(async () => {
