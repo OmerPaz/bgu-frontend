0c431b07a27748e4b305b78513bab6c1ff39af45
Omer Paz <omerpaz28@gmail.com>
2025-07-12 18:43:01 +0300

    Updated jest

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,15 +1,11 @@
 export default {
-    preset: 'ts-jest/presets/default-esm',   // transpile TS in ESM mode
-    testEnvironment: 'node',
-    extensionsToTreatAsEsm: ['.ts'],
-    moduleNameMapper: {
-      // allow importing .js in TS files without extension mismatch
-      '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
-    globals: {
-      'ts-jest': {
-        useESM: true,
-        tsconfig: 'tsconfig.json',
-      },
-    },
-  };
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+  },
+};
